b04ac1ef2896441b4bfe48b1352b75c64130d50f
gloriajw <gloriajw@none.none>
2020-11-17 18:22:15 -0500

    dispatch all the things

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,7 +1,19 @@
+# -*- coding: iso8859-15 -*-
 import os
-from flask import Flask, send_file
+import sys
 
+appdir = os.path.abspath(os.path.dirname(__file__))
+projdir = os.path.abspath(os.path.join(appdir,'..'))
+if projdir not in sys.path:
+    sys.path.append(appdir)
+    sys.path.append(projdir)
+
+from flask import Flask, send_file
+from config.project_globals import initialize_microservice
+
+# Create and init app. Now you can use app.logger and such. Woo!
 app = Flask(__name__)
+initialize_microservice(app)
 
 @app.route("/<path:filename>", methods=["GET"])
 def get_file(filename):
