31fdaedd9991bd420ec16620e4e74debb027997f
gloriajw <gloriajw@none.none>
2020-11-17 21:08:27 -0500

    generally working

--- asset/__init__.py
+++ asset/__init__.py
@@ -13,11 +13,9 @@ from config.project_globals import initialize_microservice
 
 import os
 
-from asset.views import app
+from asset.views import app,db
 
 app.config["UPLOAD_DIR"] = os.path.abspath(os.getenv("UPLOAD_DIR", "./uploads"))
 if not os.path.isdir(app.config["UPLOAD_DIR"]):
     app.logger.info(f"{app.config['UPLOAD_DIR']} doesn't exist.  Creating...")
     os.makedirs(app.config["UPLOAD_DIR"])
-
-db = initialize_microservice(app)
