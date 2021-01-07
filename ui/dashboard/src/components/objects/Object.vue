764e23a25553f2bd1a84009210f487e4b324dc5f
Hồng Phát <hongphat.js@gmail.com>
2021-01-07 23:43:18 +0700

    context menu component

--- ui/dashboard/src/components/objects/Object.vue
+++ ui/dashboard/src/components/objects/Object.vue
@@ -1,44 +1,59 @@
 <template>
-  <DragResize
-    class="object"
-    :initW="100"
-    :initH="100"
-    v-model:x="position.x"
-    v-model:y="position.y"
-    v-model:w="position.w"
-    v-model:h="position.h"
-    v-model:active="active"
-    :draggable="true"
-    :resizable="true"
-    @drag-start="dragStart"
-    @drag-end="dragEnd"
-    @resize-end="resizeEnd"
-  >
-    <Image ref="el" :src="object.src" :opacity="isDragging ? 0.5 : 1" />
-  </DragResize>
-  <Image
-    :src="object.src"
-    v-if="isDragging"
-    :style="{
-      width: position.w + 'px',
-      height: position.h + 'px',
-      position: 'fixed',
-      left: beforeDragPosition.x + 'px',
-      top: beforeDragPosition.y + 'px',
-    }"
-  />
+  <ContextMenu>
+    <template #trigger>
+      <DragResize
+        class="object"
+        :initW="100"
+        :initH="100"
+        v-model:x="position.x"
+        v-model:y="position.y"
+        v-model:w="position.w"
+        v-model:h="position.h"
+        v-model:active="active"
+        :draggable="true"
+        :resizable="true"
+        @drag-start="dragStart"
+        @drag-end="dragEnd"
+        @resize-end="resizeEnd"
+      >
+        <Image ref="el" :src="object.src" :opacity="isDragging ? 0.5 : 1" />
+      </DragResize>
+      <Image
+        :src="object.src"
+        v-if="isDragging"
+        :style="{
+          width: position.w + 'px',
+          height: position.h + 'px',
+          position: 'fixed',
+          left: beforeDragPosition.x + 'px',
+          top: beforeDragPosition.y + 'px',
+        }"
+      />
+    </template>
+    <template #context>
+      <div class="card-content">
+        <div class="columns">
+          <div class="column">First column</div>
+          <div class="column">Second column</div>
+          <div class="column">Third column</div>
+          <div class="column">Fourth column</div>
+        </div>
+      </div>
+    </template>
+  </ContextMenu>
 </template>
 
 <script>
 import DragResize from "vue3-draggable-resizable";
 import Image from "@/components/Image";
+import ContextMenu from "@/components/ContextMenu";
 import { useStore } from "vuex";
 import { reactive, ref, watch } from "vue";
 import anime from "animejs";
 
 export default {
   props: ["object"],
-  components: { DragResize, Image },
+  components: { DragResize, Image, ContextMenu },
   setup(props) {
     const store = useStore();
     const position = reactive({ ...props.object, y: 0 });
