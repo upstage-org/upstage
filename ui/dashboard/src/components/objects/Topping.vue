3277f387d6ff357eaadc3bfb6746959ee6c7d758
Hồng Phát <hongphat.js@gmail.com>
2021-02-08 18:04:01 +0700

    speech bubbles enhancement (#136)
    Co-authored-by: Hồng Phát <yhongphat.js@gmail.com>

--- ui/dashboard/src/components/objects/Topping.vue
+++ ui/dashboard/src/components/objects/Topping.vue
@@ -10,23 +10,19 @@
       v-show="active"
       class="marker fas fa-map-marker-alt fa-lg has-text-warning"
     ></i>
-    <transition @enter="enter" @leave="leave" :css="false">
+    <transition @enter="enter" @leave="leave" :css="false" appear>
       <div
         v-if="object.speak"
-        class="chat-bubble tag is-medium"
+        class="chat-bubble"
+        tabindex="-1"
         :key="object.speak"
         :style="{
-          'background-color': object.speak.backgroundColor,
-          color: object.speak.color,
+          'max-width': position.w + 'px',
+          'min-width': position.w < 100 ? '100px' : 'unset',
         }"
       >
         <span>{{ object.speak.message }}</span>
-        <i
-          class="fas fa-caret-down"
-          :style="{
-            color: object.speak.backgroundColor,
-          }"
-        ></i>
+        <i class="fas fa-caret-down"></i>
       </div>
     </transition>
   </div>
@@ -49,6 +45,8 @@ export default {
         targets: el,
         scale: [0, 1],
         rotate: [180, 0],
+        translateY: [0, "-100%"],
+        translateX: [0, "-50%"],
         complete,
       });
     };
@@ -72,12 +70,19 @@ export default {
   position: fixed;
 }
 .chat-bubble {
-  position: relative;
-  top: -15px;
-  left: -50%;
+  position: fixed;
+  border-radius: 4px;
+  padding: 0.25em 0.75em;
+  text-align: center;
+  line-height: 1em;
+  background: white;
+  box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1),
+    0 0px 0 1px rgba(10, 10, 10, 0.02);
   i.fas {
     position: absolute;
-    top: 25px;
+    left: calc(50% - 4px);
+    bottom: -8px;
+    color: white;
   }
 }
 .marker {
