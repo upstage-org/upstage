6b0dc4f614a220abaf60d01d7f6db7c3b8882862
Hồng Phát <hongphat.js@gmail.com>
2021-02-15 19:12:24 +0700

    missing mutation in schema (#140)
    Co-authored-by: Hồng Phát <yhongphat.js@gmail.com>

--- user/schema.py
+++ user/schema.py
@@ -90,7 +90,7 @@ class Query(graphene.ObjectType):
     node = relay.Node.Field()
     userList = SQLAlchemyConnectionField(User.connection)
 
-user_schema = graphene.Schema(query=Query)
+user_schema = graphene.Schema(query=Query, mutation=Mutation)
 app.add_url_rule(
     '/user_graphql/', view_func=GraphQLView.as_view("user_graphql", schema=user_schema,
     graphiql=True
