2e0d2066c79ee57cfff8ced42561fb6d23314c42
Hồng Phát <yhongphat.js@gmail.com>
2021-03-05 13:00:10 +0700

    skeleton loading

--- ui/dashboard/src/components/stage/StageTable.vue
+++ ui/dashboard/src/components/stage/StageTable.vue
@@ -1,57 +1,60 @@
 <template>
-  <table class="table" :class="{ 'is-loading': loading }">
-    <thead>
-      <tr>
-        <th><abbr title="Name (url)">Stage name</abbr></th>
-        <th><abbr title="Creator of the stage">Owner</abbr></th>
-        <th><abbr title="Duplicate/Manage stage">Actions</abbr></th>
-        <th><abbr title="Chat log">Chat log</abbr></th>
-        <th><abbr title="Screen recordings">Screen recordings</abbr></th>
-      </tr>
-    </thead>
-    <tfoot>
-      <tr></tr>
-    </tfoot>
-    <tbody>
-      <tr v-for="stage in stages" :key="stage">
-        <td>
-          <router-link to="/live" class="has-text-primary has-text-weight-bold">
-            {{ stage.name }}
-          </router-link>
-        </td>
-        <td>
-          {{ displayName(stage.owner) }}
-        </td>
-        <td class="has-text-centered">
-          <Modal>
-            <template #trigger>
-              <i class="fas fa-lg fa-cog" />
-            </template>
-            <template #header>Stage Detail</template>
-            <template #content><Detail :name="stage.name" /></template>
-            <template #footer><ActionButtons :stage="stage" /></template>
-          </Modal>
-        </td>
-        <td class="has-text-centered">
-          <i class="fas fa-lg fa-comments" />
-        </td>
-        <td class="has-text-centered">
-          <i class="fas fa-lg fa-video" />
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <DataTable :query="stageGraph.stageList" :headers="headers">
+    <template #name="{ item }">
+      <router-link to="/live" class="has-text-primary has-text-weight-bold">
+        {{ item.name }}
+      </router-link>
+    </template>
+    <template #detail="{ item }">
+      <Modal>
+        <template #trigger>
+          <i class="fas fa-lg fa-eye" />
+        </template>
+        <template #header>Stage Detail</template>
+        <template #content><Detail :name="item.name" /></template>
+        <template #footer><ActionButtons :stage="item" /></template>
+      </Modal>
+    </template>
+    <template #edit="{ item }">
+      <router-link :to="`/dashboard/stage-management/${item.id}/`">
+        <i class="fa fa-lg fa-pen has-text-black"></i>
+      </router-link>
+    </template>
+  </DataTable>
 </template>
 
 <script setup>
 import { useQuery } from "@/services/graphql/composable";
 import { stageGraph } from "@/services/graphql";
+import DataTable from "@/components/DataTable";
 import Modal from "../Modal";
 import ActionButtons from "./ActionButtons";
 import Detail from "./Detail";
 import { displayName } from "@/utils/auth";
 
-const { nodes: stages, loading } = useQuery(stageGraph.stageList);
+const headers = [
+  {
+    title: "Stage name",
+    description: "Name (url)",
+    slot: "name",
+  },
+  {
+    title: "Owner",
+    description: "Creator of the stage",
+    render: (item) => displayName(item.owner),
+  },
+  {
+    title: "Detail",
+    description: "Duplicate/Manage stage",
+    slot: "detail",
+    align: "center",
+  },
+  {
+    title: "Edit Stage",
+    slot: "edit",
+    align: "center",
+  },
+];
 </script>
 
 <style scoped>
