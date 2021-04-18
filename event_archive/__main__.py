5fb83a7e65659946abe61edf37b88747ab6c1be4
Hồng Phát <hongphat.js@gmail.com>
2021-04-18 18:09:16 +0700

    run event_archive in subprocess

---
+++ event_archive/__main__.py
@@ -0,0 +1,19 @@
+import logging
+import os
+import ssl
+import sys
+
+from system import run
+from mqtt import build_client
+import config as conf
+import actions
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+if __name__ == "__main__":
+    run()
+    mqtt_client = build_client()
+    mqtt_client.username_pw_set(conf.MQTT_USER, conf.MQTT_PASSWORD)
+    mqtt_client.connect(conf.MQTT_BROKER, conf.MQTT_PORT)
+    mqtt_client.loop_forever()
