0c8b8a0da9c7fe294e2d89d2db4bad5c85899f4e
Hồng Phát <hongphat.js@gmail.com>
2021-05-31 22:32:59 +0700

    hot fix unstable voices

--- ui/dashboard/src/services/speech/index.js
+++ ui/dashboard/src/services/speech/index.js
@@ -4,17 +4,19 @@ const { loadVoice, speak } = window.meSpeak
 
 export const avatarSpeak = (avatar, message = avatar.speak.message) => {
     const params = {}
+    const callback = () => speak(message, params) // voice got loaded assynchronous
     if (avatar.voice) {
+        console.log(avatar.voice)
         const { voice, variant, amplitude, pitch, speed } = avatar.voice
         if (isValidVoice(voice)) {
-            loadVoice(voice)
+            loadVoice(voice, callback)
         } else {
-            loadVoice(getDefaultVoice())
+            loadVoice(getDefaultVoice(), callback)
         }
         params.variant = variant
         params.amplitude = amplitude
         params.pitch = pitch
         params.speed = speed;
     }
-    speak(message, params)
+
 }
