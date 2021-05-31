09445aaedf46e197e85c01ad9721bd8207e68127
Hồng Phát <hongphat.js@gmail.com>
2021-05-31 22:36:58 +0700

    easier to read

--- ui/dashboard/src/services/speech/index.js
+++ ui/dashboard/src/services/speech/index.js
@@ -4,19 +4,19 @@ const { loadVoice, speak } = window.meSpeak
 
 export const avatarSpeak = (avatar, message = avatar.speak.message) => {
     const params = {}
-    const callback = () => speak(message, params) // voice got loaded assynchronous
     if (avatar.voice) {
         console.log(avatar.voice)
         const { voice, variant, amplitude, pitch, speed } = avatar.voice
+        params.variant = variant
+        params.amplitude = amplitude
+        params.pitch = pitch
+        params.speed = speed;
+        const callback = () => speak(message, params) // voice got loaded assynchronous
         if (isValidVoice(voice)) {
             loadVoice(voice, callback)
         } else {
             loadVoice(getDefaultVoice(), callback)
         }
-        params.variant = variant
-        params.amplitude = amplitude
-        params.pitch = pitch
-        params.speed = speed;
     }
 
 }
