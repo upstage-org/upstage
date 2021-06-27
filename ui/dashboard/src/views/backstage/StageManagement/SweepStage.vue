6e6d343798b84689383e366c9e578d6036db0f70
Hồng Phát <hongphat.js@gmail.com>
2021-06-27 17:41:18 +0700

    separated clear chat button

--- ui/dashboard/src/views/backstage/StageManagement/SweepStage.vue
+++ ui/dashboard/src/views/backstage/StageManagement/SweepStage.vue
@@ -1,10 +1,17 @@
 <template>
+  <button
+    class="button ml-2 is-warning"
+    @click="clearChat"
+    :class="{ 'is-loading': clearing }"
+  >
+    Clear Chat
+  </button>
   <button class="button ml-2 is-warning" @click="sweep">
     <template v-if="status">
       <button class="button is-warning is-loading"></button>
       <span>{{ status }}</span>
     </template>
-    <span v-else> Clear Chat & Sweep Stage</span>
+    <span v-else>Sweep Stage</span>
   </button>
 </template>
 
@@ -13,9 +20,12 @@ import { inject, ref } from "@vue/runtime-core";
 import { useMutation } from "@/services/graphql/composable";
 import { stageGraph } from "@/services/graphql";
 import { notification } from "@/utils/notification";
-import mqttService from "@/services/mqtt";
+import buildClient from "@/services/mqtt";
 import { TOPICS } from "@/utils/constants";
 import { namespaceTopic } from "@/store/modules/stage/reusable";
+
+const mqttClient = buildClient();
+
 export default {
   setup: () => {
     const stage = inject("stage");
@@ -29,13 +39,12 @@ export default {
         await mutation();
         status.value = "Clearing broker retained messages...";
         await new Promise((resolve) => {
-          mqttService.connect().on("connect", () => {
+          mqttClient.connect().on("connect", () => {
             const promisese = Object.keys(TOPICS)
               .map((key) =>
                 namespaceTopic(TOPICS[key], stage.value.fileLocation)
               )
-              .map((topic) => mqttService.sendMessage(topic, undefined, true));
-            console.log(promisese);
+              .map((topic) => mqttClient.sendMessage(topic, undefined, true));
             Promise.all(promisese).then(resolve);
           });
         });
@@ -47,7 +56,25 @@ export default {
       }
     };
 
-    return { status, sweep };
+    const clearing = ref(false);
+    const clearChat = async () => {
+      clearing.value = true;
+      await new Promise((resolve) => {
+        mqttClient.connect().on("connect", () => {
+          const topicChat = namespaceTopic(
+            TOPICS.CHAT,
+            stage.value.fileLocation
+          );
+          mqttClient
+            .sendMessage(topicChat, { clear: true }, true)
+            .then(resolve);
+        });
+      });
+      clearing.value = false;
+      notification.success(`Chat cleared successfully!`);
+    };
+
+    return { status, sweep, clearChat, clearing };
   },
 };
 </script>
