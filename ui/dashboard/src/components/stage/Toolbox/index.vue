84baba478b0b37691680fbc0ed3f5ebc490104ac
Hồng Phát <hongphat.js@gmail.com>
2021-07-03 14:33:11 +0700

    remove frame in multiframe media

--- ui/dashboard/src/components/stage/Toolbox/index.vue
+++ ui/dashboard/src/components/stage/Toolbox/index.vue
@@ -10,7 +10,7 @@
       <PanelItem name="Draw" icon="draw.svg" />
       <PanelItem name="Text" icon="text.svg" />
       <PanelItem name="Setting" icon="rotation-slider.svg" />
-      <PanelItem name="Curtain" icon="multi-frame.svg" />
+      <PanelItem name="Curtain" icon="curtain.svg" />
       <PanelItem name="Depth" icon="multi-frame.svg" />
       <a class="panel-block stage-scene-toggle">
         <span>
