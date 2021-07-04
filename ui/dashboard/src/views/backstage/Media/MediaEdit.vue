26f4e433dccd0838d510872c90e44e4f4599fc40
Hồng Phát <hongphat.js@gmail.com>
2021-07-04 17:36:34 +0700

    retro download and edit icon

--- ui/dashboard/src/views/backstage/Media/MediaEdit.vue
+++ ui/dashboard/src/views/backstage/Media/MediaEdit.vue
@@ -2,7 +2,7 @@
   <Modal width="100%" height="100%">
     <template #trigger>
       <a class="button is-light is-small">
-        <i class="fas fa-pen has-text-primary" aria-hidden="true"></i>
+        <Icon src="edit.svg" />
       </a>
     </template>
     <MediaForm :media="media" @complete="refresh()" />
@@ -12,11 +12,13 @@
 
 <script>
 import Modal from "@/components/Modal";
+import Icon from "@/components/Icon";
 import MediaForm from "./MediaForm";
 
 export default {
   components: {
     Modal,
+    Icon,
     MediaForm,
   },
   props: {
