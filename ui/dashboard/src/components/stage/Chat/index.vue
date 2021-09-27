7cbe9ccdc7fec260c1c6263e4f8cef68bded348a
Hồng Phát <hongphat.js@gmail.com>
2021-09-27 22:01:34 +0700

    fix issue 584

--- ui/dashboard/src/components/stage/Chat/index.vue
+++ ui/dashboard/src/components/stage/Chat/index.vue
@@ -17,7 +17,7 @@
       <div class="actions">
         <Reaction v-if="collapsed" />
         <button
-          class="chat-setting button is-rounded is-light"
+          class="chat-setting button is-rounded is-outlined"
           @click="collapsed = !collapsed"
           :data-tooltip="collapsed ? 'Maximise' : 'Minimise'"
           :key="collapsed"
@@ -28,7 +28,7 @@
           </span>
         </button>
         <button
-          class="chat-setting button is-rounded is-light"
+          class="chat-setting button is-rounded is-outlined"
           @click="openChatSetting"
           data-tooltip="Settings"
         >
