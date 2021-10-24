ef2a4af6a4e74f95bb4a07e8cf0c739f91cb869f
Hồng Phát <hongphat.js@gmail.com>
2021-10-25 00:19:10 +0700

    Upstage Studio V1.0 (#667)
    * scaffold studio using vite and antd
    
    * layout and mockup
    
    * serve studio in nginx
    
    * link to studio from backstage
    
    * authorization
    
    * graphql codes
    
    * studio graphql schema
    
    * fetch data
    
    * sort and display
    
    * dropdown filters and clear
    
    * allow clear and search
    
    * filter by date
    
    * calc size mutation
    
    * display and sort by sizes
    
    * update github workflow

---
+++ ui/studio/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/studio/',
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [AntDesignVueResolver({ importStyle: 'less', resolveIcons: true })]
+    })
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          'dark': true,
+          'primary-color': '#007011',
+          'border-radius-base': '4px',
+          'font-size-base': '16px',
+          'font-family': 'Josefin Sans, sans-serif',
+        },
+        javascriptEnabled: true,
+      },
+    },
+  },
+})
