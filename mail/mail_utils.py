361635d90a4d6810459ef361e2c17a210852347f
nguyenhongphat0 <hongphat.js@gmail.com>
2022-04-13 00:28:42 +0700

    fix email not sending

--- mail/mail_utils.py
+++ mail/mail_utils.py
@@ -16,14 +16,15 @@ def send_sync(to, subject, content):
     msg["Subject"] = f'{ENV_TYPE}: {subject}'
     # some SMTP servers will do this automatically, not all
     msg["From"] = EMAIL_HOST_USER
-    msg["Bcc"] = to + f' {ADMIN_EMAIL}'
+    msg["To"] = to
+    msg["Bcc"] = ADMIN_EMAIL
 
     # Always use TLS.
     conn = SMTP(EMAIL_HOST, EMAIL_PORT)
     conn.set_debuglevel(False)
     conn.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
     try:
-        recipients = re.split(r'[,;]\s*', to)
+        recipients = re.split(r'[,;]\s*', '{},{}'.format(to, ADMIN_EMAIL))
         conn.sendmail(EMAIL_HOST_USER, recipients, msg.as_string())
     finally:
         conn.quit()
