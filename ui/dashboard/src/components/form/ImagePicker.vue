d6ca0adb47b7f3e9fc3dffba95684aa9a43a60db
Giang <giang281197@gmail.com>
2022-05-12 23:58:39 +0700

    create image type curtain and add filter image

--- ui/dashboard/src/components/form/ImagePicker.vue
+++ ui/dashboard/src/components/form/ImagePicker.vue
@@ -1,24 +1,39 @@
 <template>
   <modal>
     <template #trigger>
-      <Asset
-        class="clickable"
-        v-if="modelValue"
-        :asset="{
-          src: modelValue,
-        }"
-      />
+      <Asset class="clickable" v-if="modelValue" :asset="{
+        src: modelValue,
+      }" />
       <button v-else class="button">Choose an image</button>
     </template>
     <template #header> Choose an existing image or upload new </template>
     <template #content="{ closeModal }">
-      <div class="columns is-multiline">
+      <div class="columns is-multiline ">
         <div class="colum px-3 pt-1">
           <MediaUpload />
         </div>
       </div>
       <Loading v-if="loading" />
       <div v-else class="columns is-multiline">
+        <div class="column is-12 ">
+          <div class="columns">
+            <div class="column panel-heading">
+              <p class="control has-icons-left">
+                <input class="input" type="text" placeholder="Search Media" v-model="filter.name" />
+                <span class="icon is-left">
+                  <i class="fas fa-search" aria-hidden="true"></i>
+                </span>
+              </p>
+            </div>
+            <Dropdown class="column" v-model="filter.mediaType" :data="types"
+              :render-label="(type) => type.name === 'media' ? 'All types' : type.name" :render-value="(type) => type"
+              style="width: 100%" fixed placeholder="All Types" />
+            <Dropdown class="column" v-model="filter.owner" :data="users" :render-label="(user) => displayName(user)"
+              :render-value="(user) => user" style="width: 100%" fixed placeholder="All Users" />
+            <Dropdown class="column" v-model="filter.stage" :data="stages" :render-label="(stage) => stage.name"
+              :render-value="(stage) => stage" style="width: 100%" fixed placeholder="All Stages" />
+          </div>
+        </div>
         <div class="column is-3" v-for="item in availableImages" :key="item">
           <div class="card-image clickable" @click="select(item, closeModal)">
             <Asset :asset="item" />
@@ -36,22 +51,20 @@ import Asset from "@/components/Asset";
 import { stageGraph } from "@/services/graphql";
 import { useQuery } from "@/services/graphql/composable";
 import MediaUpload from "@/views/backstage/Media/MediaUpload";
-import { computed, provide } from "@vue/runtime-core";
+import { computed, provide, reactive } from "@vue/runtime-core";
+import Dropdown from "./Dropdown";
+import { displayName } from "@/utils/auth";
 export default {
   props: ["modelValue"],
   emits: ["update:modelValue"],
-  components: { Modal, Loading, Asset, MediaUpload },
+  components: { Modal, Loading, Asset, MediaUpload, Dropdown },
   setup: (props, { emit }) => {
     const {
       loading,
       nodes: mediaList,
       refresh,
     } = useQuery(stageGraph.mediaList);
-    const availableImages = computed(() =>
-      mediaList.value.filter((m) =>
-        ["avatar", "prop", "backdrop", "shape"].includes(m.assetType.name)
-      )
-    );
+    const type_dis = ["avatar", "prop", "backdrop", "shape", "curtain"];
     provide("refresh", refresh);
 
     const select = (item, closeModal) => {
@@ -59,7 +72,67 @@ export default {
       closeModal();
     };
 
-    return { loading, availableImages, select };
+    const filter = reactive({
+      name: null,
+      mediaType: null,
+      owner: null,
+      stage: null
+    });
+
+    const availableImages = computed(() => {
+      let list = mediaList.value;
+      list = list.filter((m) => type_dis.includes(m.assetType.name))
+      if (filter.name) {
+        list = list.filter((media) => media.name.toLowerCase().includes(filter.name.toLowerCase()));
+      }
+      if (filter.mediaType && filter.mediaType.id) {
+        list = list.filter((media) => media.assetType.name === filter.mediaType.name);
+      }
+      if (filter.owner && filter.owner.id) {
+        list = list.filter((media) => media.owner.username === filter.owner.username);
+      }
+      if (filter.stage && filter.stage.id) {
+        list = list.filter((media) => media.stages.find((s) => s.id === filter.stage.dbId));
+      }
+      return list;
+    });
+
+    const { nodes: typesData } = useQuery(stageGraph.assetTypeList);
+
+    const types = computed(() => {
+      const list = [];
+      list.push({ id: null, name: "All Types" })
+      typesData.value.filter((m) => type_dis.includes(m.name)).forEach(type => list.push(type))
+      return list
+    }
+
+    );
+
+    const users = computed(() => {
+      let list = [];
+      list.push({ id: null, displayName: "All Users" })
+      if (mediaList.value) {
+        mediaList.value.forEach(({ owner }) => {
+          if (!list.some((user) => user.username === owner.username)) {
+            list.push(owner);
+          }
+        });
+      }
+      return list;
+    });
+
+    const { nodes: stagesData } = useQuery(stageGraph.stageList);
+    const stages = computed(() => {
+      let list = [];
+      list.push({ id: null, name: "All Stages" })
+      if (stagesData.value) {
+        stagesData.value.forEach(stage => list.push(stage));
+      }
+      return list;
+    }
+    );
+
+    return { loading, availableImages, select, filter, types, users, displayName, stages };
   },
 };
 </script>
