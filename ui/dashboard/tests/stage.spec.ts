228228e1561a01e18dd5246c3ceb58deb67a373c
Giang <giang281197@gmail.com>
2022-07-18 21:19:02 +0700

    wait 3 minute

--- ui/dashboard/tests/stage.spec.ts
+++ ui/dashboard/tests/stage.spec.ts
@@ -6,7 +6,8 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('Demo Stage', () => {
   test('should have live status', async ({ page }) => {
-    const status = await page.locator('.status-text').innerText();
+    await new Promise(f => setTimeout(f, 3000));
+    const status = await page.locator('.status-text').innerText();    
     await expect(status.toUpperCase()).toEqual('LIVE')
   })
 })
