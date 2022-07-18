98afaa5740d9a40be86236b332a9ce2611621cb5
Giang <giang281197@gmail.com>
2022-07-18 21:25:10 +0700

    fix playwright test

--- ui/dashboard/tests/stage.spec.ts
+++ ui/dashboard/tests/stage.spec.ts
@@ -2,11 +2,11 @@ import test, { expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("https://dev-app1.upstage.live/demo");
+  await new Promise(f => setTimeout(f, 3000));
 })
 
 test.describe('Demo Stage', () => {
   test('should have live status', async ({ page }) => {
-    await new Promise(f => setTimeout(f, 3000));
     const status = await page.locator('.status-text').innerText();    
     await expect(status.toUpperCase()).toEqual('LIVE')
   })
