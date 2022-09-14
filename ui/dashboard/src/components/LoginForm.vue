e7f72bcca228614264b9f53be5728e8009c106f7
giang <giang281197@gmail.com>
2022-09-15 01:09:25 +0700

    Fetch Stages after login

--- ui/dashboard/src/components/LoginForm.vue
+++ ui/dashboard/src/components/LoginForm.vue
@@ -155,6 +155,7 @@ const submit = () => {
   store
     .dispatch("auth/login", user)
     .then(() => {
+      store.dispatch("cache/fetchStages");
       emit("success");
     })
     .finally(() => {
