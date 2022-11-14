14f89a9d88d9e6445470fa4c46350ef2f91ee33f
Liew <liew2018@gmail.com>
2022-11-14 12:42:28 +0700

    change "manage stage" to "manage"

--- ui/dashboard/src/components/stage/StageTable.vue
+++ ui/dashboard/src/components/stage/StageTable.vue
@@ -105,7 +105,7 @@ export default {
         align: "center",
       },
       {
-        title: "Manage Stage",
+        title: "Manage",
         slot: "manage",
         align: "center",
       },
