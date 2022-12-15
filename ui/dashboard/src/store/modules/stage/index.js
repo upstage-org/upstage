2e9cc4b9cfb918e9f3453a172d3b9642608dcca8
tunguyentk <117065175+tunguyentk@users.noreply.github.com>
2022-12-15 09:46:24 +0700

    make clear chat to clear player and public chat on stage- clear all chat in clear chat button in stage management (#1042)

--- ui/dashboard/src/store/modules/stage/index.js
+++ ui/dashboard/src/store/modules/stage/index.js
@@ -82,6 +82,7 @@ export default {
         isSavingScene: false,
         isLoadingScenes: false,
         showPlayerChat: false,
+        showClearChatSetting:false,
         lastSeenPrivateMessage: localStorage.getItem('lastSeenPrivateMessage') ?? 0,
         runningStreams: [],
         masquerading: false,
@@ -278,6 +279,9 @@ export default {
         },
         CLEAR_CHAT(state) {
             state.chat.messages.length = 0
+        },
+        CLEAR_PLAYER_CHAT(state) {
+            state.chat.privateMessages.length = 0
         },
         REMOVE_MESSAGE(state, id) {
             state.chat.messages = state.chat.messages.filter(m => m.id !== id)
@@ -504,6 +508,9 @@ export default {
         SET_SHOW_PLAYER_CHAT(state, value) {
             state.showPlayerChat = value
         },
+        SET_SHOW_CLEAR_CHAT_SETTINGS(state, value) {
+            state.showClearChatSetting = value
+        },
         TAG_PLAYER(state, player) {
             state.chat.privateMessage += `@${player.nickname.trim()}`
         },
@@ -699,6 +706,10 @@ export default {
         handleChatMessage({ commit, state, rootGetters, dispatch }, { message }) {
             if (message.clear) {
                 commit('CLEAR_CHAT')
+                return;
+            }
+            if (message.clearPlayerChat) {
+                commit('CLEAR_PLAYER_CHAT')
                 return;
             }
             if (message.remove) {
@@ -1005,6 +1016,14 @@ export default {
                 state.model.permission = permission
             }
         },
+        async loadPermission({ state , commit}) {
+            const permission = await stageGraph.loadPermission(state.model.fileLocation)
+            if(permission=="owner"||permission=="editor"){
+              commit('SET_SHOW_CLEAR_CHAT_SETTINGS',true)
+            }else{
+                commit('SET_SHOW_CLEAR_CHAT_SETTINGS',false)
+            }
+        },
         async reloadScenes({ state }) {
             state.isLoadingScenes = true
             const scenes = await stageGraph.loadScenes(state.model.fileLocation)
@@ -1163,6 +1182,9 @@ export default {
         },
         clearChat() {
             mqtt.sendMessage(TOPICS.CHAT, { clear: true })
+        },
+        clearPlayerChat() {
+            mqtt.sendMessage(TOPICS.CHAT, { clearPlayerChat: true })
         },
         removeChat(action, messageId) {
             mqtt.sendMessage(TOPICS.CHAT, { remove: messageId })
