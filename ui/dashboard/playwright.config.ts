c4b9b8212cc71d9665be0202bf41e4031440a8c3
Jamse Liew <liew2018@gmail.com>
2022-12-19 13:55:37 +0700

    debug

--- ui/dashboard/playwright.config.ts
+++ ui/dashboard/playwright.config.ts
@@ -40,6 +40,7 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    channel: 'chrome',
   },
 
   /* Configure projects for major browsers */
