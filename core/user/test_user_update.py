4d0805dc339432c2608d9b457c31661184258ccd
Willadsen, Gloria (CNTR) <Gloria.Willadsen@Intelsat.com>
2023-12-19 14:30:31 -0500

    Modelo 2.0.4 (#1078)
    * Update devapp1.yml
    
    New release
    
    * Update playwright-dev.yml
    
    New release
    
    * Update package.json
    
    * dev chgs to reflect new structure
    
    * Bump minimatch from 3.0.4 to 3.1.2 in /studio (#1079)
    
    Bumps [minimatch](https://github.com/isaacs/minimatch) from 3.0.4 to 3.1.2.
    - [Release notes](https://github.com/isaacs/minimatch/releases)
    - [Changelog](https://github.com/isaacs/minimatch/blob/main/changelog.md)
    - [Commits](https://github.com/isaacs/minimatch/compare/v3.0.4...v3.1.2)
    
    ---
    updated-dependencies:
    - dependency-name: minimatch
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Undo Integrate PlayWright (#1080)
    
    * Bump minimatch from 3.0.4 to 3.1.2 in /studio (#1079)
    
    Bumps [minimatch](https://github.com/isaacs/minimatch) from 3.0.4 to 3.1.2.
    - [Release notes](https://github.com/isaacs/minimatch/releases)
    - [Changelog](https://github.com/isaacs/minimatch/blob/main/changelog.md)
    - [Commits](https://github.com/isaacs/minimatch/compare/v3.0.4...v3.1.2)
    
    ---
    updated-dependencies:
    - dependency-name: minimatch
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * remove the playwright badge
    
    * un integrate playwright
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Issue 1089 Unique email address (#1105)
    
    * make email column unique
    
    * ignore local configurations & secrets
    
    * open stages in new tab (#1106)
    
    * Issue 1097 Open stages in new tab (cont.) (#1108)
    
    * open stages in new tab
    
    * open as new tab from stage management
    
    * Issue 1089 Update existed user email (#1109)
    
    * test update existed email mutation
    
    * raise error when email address is belongs to another user
    
    * fix unsupported operand types for |
    
    * lock sqlalchemy version
    
    * Issue 1093 Stage table actions (#1110)
    
    * quick login for developers on studio
    
    * fix reactivity not working
    
    * update stage inside studio
    
    * update status & visibility mutation
    
    * table visibility & status toggle
    
    * no split layout (#1111)
    
    * scaffold script enhancement (#1112)
    
    Co-authored-by: gloriajw <aagg@comcast.net>
    
    * consistent open stage behavior (#1113)
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: gloriajw <aagg@comcast.net>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Hồng Phát <hongphat.js@gmail.com>

---
+++ core/user/test_user_update.py
@@ -0,0 +1,90 @@
+from graphene.test import Client
+from flask import request
+from core import app
+from .schema import user_schema
+from time import time
+
+
+def get_access_token():
+    with app.test_request_context():
+        client = Client(user_schema)
+        auth = client.execute(
+            """
+            mutation AuthUser($username: String, $password: String) {
+                authUser(username: $username, password: $password) {
+                    accessToken
+                    refreshToken
+                }
+            }
+        """,
+            variable_values={
+                "username": "automation_test",
+                "password": "makeupstagegreatagain",
+            },
+        )
+        accessToken = auth["data"]["authUser"]["accessToken"]
+        return accessToken
+
+
+def execute_update_email(newEmail):
+    with app.test_request_context():
+        client = Client(user_schema)
+        request.headers = {"X-Access-Token": get_access_token()}
+
+        executed = client.execute(
+            """
+                mutation UpdateUser($id: ID!, $displayName: String, $firstName: String, $lastName: String, $email: String, $password: String, $active: Boolean, $role: Int, $uploadLimit: Int) {
+                  updateUser(inbound: {id: $id, displayName: $displayName, firstName: $firstName, lastName: $lastName, email: $email, password: $password, active: $active, role: $role, uploadLimit: $uploadLimit}) {
+                    user {
+                      ...userFragment
+                    }
+                  }
+                }
+
+                fragment userFragment on User {
+                  id
+                  dbId
+                  username
+                  firstName
+                  lastName
+                  displayName
+                  email
+                  active
+                  createdOn
+                  role
+                  uploadLimit
+                  intro
+                }
+                """,
+            variable_values={
+                "id": "VXNlcjoxNjY=",
+                "dbId": 166,
+                "username": "teddy1",
+                "firstName": "",
+                "lastName": "",
+                "displayName": "",
+                "email": newEmail,
+                "active": True,
+                "createdOn": "2023-07-22T16:11:41.103693",
+                "role": 4,
+                "uploadLimit": 1048576,
+                "intro": "",
+            },
+        )
+        return executed
+
+
+def test_update_email_existed():
+    newEmail = "hongphat.js@gmail.com"
+    executed = execute_update_email(newEmail)
+    assert (
+        executed["errors"][0]["message"]
+        == "This email address already belongs to another user!"
+    )
+
+
+def test_update_email_success():
+    timestamp = str(int(time()))
+    newEmail = f"test.{timestamp}@gmail.com"
+    executed = execute_update_email(newEmail)
+    assert executed["data"]["updateUser"]["user"]["email"] == newEmail
