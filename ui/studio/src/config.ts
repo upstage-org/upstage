6e8a59ce62e58f03f9a330536306bbebd459d62e
Gloria W <gwcorresp01@gmail.com>
2023-12-20 09:54:00 -0500

    migrate from app1 to upstage.live (#671)

--- ui/studio/src/config.ts
+++ ui/studio/src/config.ts
@@ -2,6 +2,7 @@ const configs = {
   MODE: import.meta.env.MODE as 'development' | 'production',
   UPSTAGE_URL: import.meta.env.VITE_APP_UPSTAGE_URL ?? window.location.origin,
   GRAPHQL_ENDPOINT: import.meta.env.VITE_APP_GRAPHQL_ENDPOINT as string,
+  STATIC_ASSETS_ENDPOINT: import.meta.env.VITE_APP_STATIC_ASSETS_ENDPOINT as string,
   ALLOWED_EXTENSIONS: {
     IMAGE: '.svg,.jpg,.jpeg,.png,.gif',
     AUDIO: '.wav,.mpeg,.mp3,.aac,.aacp,.ogg,.webm,.flac,.m4a',
