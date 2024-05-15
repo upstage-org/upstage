c2ede41957b9083c64f765762fbedb72f61630d2
webdev604 <webdev.604@hotmail.com>
2024-05-15 23:22:57 +0800

    need interim pop-up message #1274

--- studio/src/views/stages/StageManagement/SweepStage.vue
+++ studio/src/views/stages/StageManagement/SweepStage.vue
@@ -1,30 +1,51 @@
 <template>
-  <button class="button ml-2 is-warning" @click="sweep">
+  <button class="button ml-2 is-warning" @click="onConfirm">
     <template v-if="status">
       <button class="button is-warning is-loading"></button>
       <span>{{ status }}</span>
     </template>
-    <span v-else
-      ><slot>{{ $t("sweep_stage") }}</slot></span
-    >
+    <span v-else></span>
+    <slot>{{ $t("sweep_stage") }}</slot>
   </button>
 </template>
 
 <script>
-import { inject, ref } from "vue";
+import { inject, ref, createVNode } from "vue";
 import { message } from "ant-design-vue";
 import { useMutation } from "services/graphql/composable";
 import { stageGraph } from "services/graphql";
 import { useClearStage } from "components/stage/composable";
+import { Modal } from "ant-design-vue";
 
 export default {
-  setup: () => {
+  props: {
+    archive: Boolean,
+  },
+  setup: (props) => {
     const stage = inject("stage");
     const refresh = inject("refresh");
     const status = ref();
     const { mutation } = useMutation(stageGraph.sweepStage, {
       id: stage.value.id,
     });
+
+    const onConfirm = () => {
+      Modal.confirm({
+        title: "Confirm",
+        content: createVNode(
+          "div",
+          { style: "color: black; white-space: pre-line;" },
+          props.archive ? "Archiving will create a replay recording and chat files from the stage since it was last archived. It will also sweep the stage and start a new recording. Sweeping the stage removes all media items from the stage, including text and drawings. Media assigned to the stage will still be available in the toolbars, as will any scenes that have been saved.\n Do you want to archive now?" :
+            "Sweeping the stage removes all media items from the stage, including text and drawings. Media assigned to the stage will still be available in the toolbars, as will any scenes that have been saved.\n Do you want to sweep the stage?",
+        ),
+        onOk() {
+          sweep();
+        },
+        okButtonProps: {
+          danger: true,
+        },
+      });
+    };
 
     const sweep = async () => {
       try {
@@ -44,7 +65,7 @@ export default {
       }
     };
 
-    return { status, sweep };
+    return { status, sweep, onConfirm };
   },
 };
 </script>
